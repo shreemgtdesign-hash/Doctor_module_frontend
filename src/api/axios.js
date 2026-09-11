b334f09940d9457488dba36123a1f5a0c0e8e280
musthamirzu <musthamirzu15@gmail.com>
2026-09-11 19:05:07 +0530

    notification

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://72.62.78.53:5000/api",
+  baseURL: "https://72.62.78.53:8443/api",
   headers: {
     "Content-Type": "application/json",
   },
